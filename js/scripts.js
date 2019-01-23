be60643bb1ffb9909633747306d22d5c8a3c7a9a
Carsten Barylski <carsten.barylski@posteo.de>
2019-01-23 13:48:34 +0100

    JavaScript-Code cleaned

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-//Twitter-Button
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-//Facebook-Button
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.12';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 //Google Maps
 var map;
 var position;
@@ -42,22 +30,6 @@ function placeMarkerAndPanTo(latLng, map) {
 
 $(document).ready(function(){
 
-  $(function () {
-    document.getElementById("message-box").style.backgroundColor = "lightblue";
-  });
-
-  $("#message-box").on("keyup", function() {
-      console.log("keyup happened");
-      var charCountm = $("#message-box").val().length;
-      console.log(charCountm);
-      $("#char-count").html(charCountm);
-      if(charCountm > 50) {
-          $("#char-count").css("color", "red");
-      } else {
-          $("#char-count").css("color", "black");
-      };
-  });
-
   //Smooth Scrolling
 
   var $root = $('html, body');
@@ -70,49 +42,6 @@ $(document).ready(function(){
         window.location.hash = href;
       });
     }
-    return false;
-  });
-
-  //Tooltips
-
-  $(function () {
-    $('#item1').tooltip();
-  });
-
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-
-  $("#submit-button").on('click', function() {
-    var charCountn = $("#name").val().length;
-    if(charCountn === 0) {
-        $("#name").css("border", "2px solid red");
-    } else {
-        $('#visible-name').html('Thank You.');
-        $('#name').hide();
-    };
-    var charCountp = $("#phone").val().length;
-    if(charCountp === 0) {
-        $("#phone").css("border", "2px solid red");
-    } else {
-        $('#visible-phone').html('Thank You.');
-        $('#phone').hide();
-    };
-    var charCounte = $("#email").val().length;
-    if(charCounte === 0) {
-        $("#email").css("border", "2px solid red");
-    } else {
-      $('#visible-email').html('Thank You.');
-      $('#email').hide();
-    };
-    var charCountm = $("#message-box").val().length;
-    if(charCountm === 0) {
-        $("#message-box").css("border", "2px solid red");
-    } else {
-        var comment = $('#message-box').val();
-        $('#visible-comment').html('Thank you. We have reveived your message and will get in contact with you soon!');
-        $('#message-box').hide();
-    };
     return false;
   });
 
